63701556a2cbea166f493a1d3c154338248f98de
Misterless <healwinds@gmail.com>
2022-04-06 16:48:35 +0900

    security 설정 완료

---
+++ src/main/java/sitte/metacoding/blogv4/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package sitte.metacoding.blogv4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //이거없으면 포스트맨 테스트 못해요
+        http.authorizeRequests()
+        .antMatchers("/s/**").authenticated()
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/loginForm")
+        .defaultSuccessUrl("/");
+       // super.configure(http);
+    }
+
+    
+}
