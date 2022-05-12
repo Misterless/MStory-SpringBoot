98e98ca70033057fa40ccb924676c359598dafce
Misterless <healwinds@gmail.com>
2022-05-12 14:52:17 +0900

    카테고리 에러처리및 null값들어감확인 조속히 수정 예정

--- src/main/java/site/metacoding/blogv4/domain/category/CategoryRepository.java
+++ src/main/java/site/metacoding/blogv4/domain/category/CategoryRepository.java
@@ -2,6 +2,6 @@ package site.metacoding.blogv4.domain.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository{
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
     
 }
